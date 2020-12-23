25d9762c2a6b55a1abc12aa7b5bd70fb1e41f443
Fun Coder <codewhilecoding@gmail.com>
2020-12-22 20:32:54 -0500

    Bring back the product with empty check

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -57,7 +57,34 @@ function HomeScreen(props) {
       ) : error ? (
         <div>{error}</div>
       ) : (
-        <div> Loaded something? </div>
+        <ul className="products">
+          {
+            (products === undefined || products.length === 0) ? <div> no items found </div> :          
+              products.map((product) => (
+                <li key={product._id}>
+                  <div className="product">
+                    <Link to={'/product/' + product._id}>
+                      <img
+                        className="product-image"
+                        src={product.image}
+                        alt="product"
+                      />
+                    </Link>
+                    <div className="product-name">
+                      <Link to={'/product/' + product._id}>{product.name}</Link>
+                    </div>
+                    <div className="product-brand">{product.brand}</div>
+                    <div className="product-rating">
+                      <Rating
+                        value={product.rating}
+                        text={product.numReviews + ' reviews'}
+                      />
+                   </div>
+                  </div>
+                </li>
+              ))
+          }
+        </ul>
       )}
     </>
   );
